ec7fe9a2d0a9e69374f0c85266b6e3711e60bc8f
Kelvin Nii Lartey <niila@MacBook-Air-110.local>
2025-03-05 21:28:09 -0500

    5th commit, I am goging to run my nasa file in the production environment to see if it runs better there

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 all: 
 	g++ -Wall NasaLaunchAnalysis.cpp -o nasa
+	g++ -g -Wall TimeCode.cpp TimeCodeTests.cpp -o tct
+	g++ -g -Wall TimeCode.cpp PaintDryTimer.cpp -o pdt
 	
 run:
 	./nasa
+	./tct
+	./pdt
 
 	
 try:all run
--- NasaLaunchAnalysis.cpp
+++ NasaLaunchAnalysis.cpp
@@ -1,92 +1,76 @@
-// Author; Kelvin Lartey
-
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <sstream>
 #include "TimeCode.cpp"
+#include "TimeCode.h"
 
 using namespace std;
 
-// storing my strings
-
-
-vector<string> split(string line, char delimiter){
-    // to store my string sseprated by commas. 
-
+// Function to split a string based on a delimiter
+vector<string> split(string line, char delimiter) {
     vector<string> results;
     string temp_string;
     
-    for (int i = 0; i < line.length(); i ++){
-        if (line[i] != delimiter){
+    for (int i = 0; i < line.length(); i++) {
+        if (line[i] != delimiter) {
             temp_string += line[i];
-        }
-        else{
+        } else {
             results.push_back(temp_string);
-            temp_string = "";
+            temp_string = ""; // Reset temp string
         }
-
     }
     if (!temp_string.empty()) {
-        results.push_back(temp_string);
+        results.push_back(temp_string); // Ensure last element gets added
     }
-
     return results;
-
 }
 
+// Function to extract time from a line
 TimeCode parse_line(string line) {
-
-    string time;
-    vector<string> separate = split (line, ',');
-
-    // okay so a traker is needed to stop it when it reaches a time when it is not true
-    bool found_time = false;
-    //using the find function ; https://cplusplus.com/reference/string/string/find/
-    for (int i= 0; i < separate.size(); i++){
-
-        // Using UTC as a constant that is always there when a time is available then I can 
-        // use that to extract the time infront of it.
-        if (separate[i].find("UTC") != string::npos){
-            vector<string> timeParts = split(separate[i], ' ');
+    vector<string> separate = split(line, ',');
+    string time = "";
+    
+    // Loop through the columns to find the one containing time
+    for (int i = 0; i < separate.size(); i++) {
+        if (separate[i].find("UTC") != string::npos) { // Checking if 'UTC' is in the string
+            vector<string> timeParts = split(separate[i], ' '); // Splitting by space
             
             for (int j = 0; j < timeParts.size(); j++) {
-                if (timeParts[j].find(":") != string::npos) {  
-                    time = timeParts[j];  
-                    found_time = true;
+                if (timeParts[j].find(":") != string::npos) { // Finding the actual time
+                    time = timeParts[j];
                     break;
                 }
             }
-        }    
+            break; // Exit loop after finding time
+        }
     }
     
-    // When 000 are added it increases the size of the vector 
-    // meaning the total average is lower.
-    if (!found_time || time.empty()) {
-        // Debugging purposes. 
-        //cout << "Skipped line: " << line << endl;
-        return TimeCode(0, 0, 0);  
-        
-    }
-        
-    // convert the times to timecode. 
-    vector<string> separate_parts = split(time, ':');
-    if (separate_parts.size() < 2) {
-    // For cases when the time value is empty
+    if (time.empty()) { // If no time was found, return default TimeCode
         return TimeCode(0, 0, 0);
     }
-
-    int hours = stoi(separate_parts[0]);
-    int minutes = stoi(separate_parts[1]);
-
-        return TimeCode(hours, minutes, 0);
+    
+    // Ensure time string is trimmed of spaces
+    time.erase(remove(time.begin(), time.end(), ' '), time.end());
+    
+    // Split the time string into hours and minutes (only HH:MM format is expected)
+    vector<string> time_parts = split(time, ':');
+    if (time_parts.size() != 2) { // Ensure we only get HH:MM format
+        return TimeCode(0, 0, 0);
+    }
+    
+    int hours = stoi(time_parts[0]); // Convert string to integer
+    int minutes = stoi(time_parts[1]);
+    int seconds = 0; // Since dataset does not contain HH:MM:SS, default seconds to 0
+    
+    return TimeCode(hours, minutes, seconds); // Return extracted time
 }
 
 int main() {
     string file_name;
     cout << "Enter file name: ";
     cin >> file_name;
-    vector <TimeCode> my_time;
+    vector<TimeCode> my_time; // Vector to store extracted time values
 
     ifstream inFS(file_name);
     if (!inFS.is_open()) {
@@ -95,41 +79,37 @@ int main() {
     }
 
     string line;
-    int skipped_zeroes = 0;
-    while (getline(inFS, line)) {  
-        TimeCode tc = parse_line(line);  
-        if (tc.GetHours() == 0 && tc.GetMinutes() == 0 && tc.GetSeconds() == 0) {
-            skipped_zeroes++;
-        }
-        else{
+    int skipped_zeroes = 0; // Counter for invalid entries
+    int valid_data_points = 0; // Counter for valid data points
+    getline(inFS, line); // Skip the header line
+    
+    while (getline(inFS, line)) {
+        TimeCode tc = parse_line(line);
+        if (tc.GetHours() == 0 && tc.GetMinutes() == 0) {
+            skipped_zeroes++; // Count invalid entries
+        } else {
             my_time.push_back(tc);
-        }  
+            valid_data_points++; // Count valid entries
+        }
     }
-
-    inFS.close();  
-
-    //cout << "Skipped time values that were 00:00:00: " << skipped_zeroes << endl;
+    
+    inFS.close(); // Close the file when done
 
     if (my_time.empty()) {
         cout << "No valid launch times found. Cannot compute an average." << endl;
         return 1;
     }
 
-    // Now to average my time out
+    // Compute the average launch time
     long long unsigned int total_time = 0;
     for (int i = 0; i < my_time.size(); i++) {
-        unsigned int hours = my_time[i].GetHours();
-        unsigned int minutes = my_time[i].GetMinutes();
-        unsigned int seconds = my_time[i].GetSeconds();
-
-        total_time += my_time[i].ComponentsToSeconds(hours, minutes, seconds);
+        total_time += my_time[i].ComponentsToSeconds(my_time[i].GetHours(), my_time[i].GetMinutes(), 0); // Always set seconds to 0
     }
-    long long unsigned int average_seconds = total_time / my_time.size();
-    // Convert back because timecode does not accept t seconds. 
-    TimeCode average_time(0, 0, average_seconds);
-    //cout << "Total time in seconds: " << total_time << endl;
-    //cout << "Number of valid data points: " << my_time.size() << endl;
-    cout << my_time.size() << " data points." << endl;
-    cout << "Average: " << average_time.ToString() << endl;
+
+    long long unsigned int average_seconds = total_time / my_time.size(); // Compute average
+    TimeCode average_time(0, 0, average_seconds); // Convert back to TimeCode format
+
+    cout << valid_data_points << " data points." << endl;
+    cout << "Average: " << average_time.ToString() << endl; // Print average launch time
     return 0;
 }
